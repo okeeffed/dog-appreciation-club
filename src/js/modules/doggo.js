42bb417c9886e29f1c7404391dccd8404ee8af93
okeeffed <okeeffe_d@icloud.com>
2017-09-02 19:08:03 +1000

    Update the doggo info

--- src/js/modules/doggo.js
+++ src/js/modules/doggo.js
@@ -1,11 +1,20 @@
 import axios from 'axios';
 
 export default class Doggo {
+	/**
+	 * Initialise the page
+	 * @return {Doggo} Return Doggo instance
+	 */
 	init() {
 		this.fetchDoggo();
-
+		return this;
 	}
 
+	/**
+	 * Fetch the dog info from the API and display it
+	 * on the page
+	 * @return {Doggo} Return Doggo instance
+	 */
 	fetchDoggo() {
 		axios.get('http://localhost:4090/api/v1/dog')
 			.then(res => {
